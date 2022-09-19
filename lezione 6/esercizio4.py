85b4b14f31273a60e85f79a8f6173747c5fa8273
alessandro.lobuono <lobuono@olomedia.it>
2022-09-19 16:09:06 +0200

    Lezione 6

--- lezione 6/esercizio4.py
+++ lezione 6/esercizio4.py
@@ -4,7 +4,11 @@ list = [randrange(18, 31),randrange(18, 31),randrange(18, 31),randrange(18, 31),
 # Stampo la lista
 print('Lista di voti: ', list)
 lower = 31
+higher = 0
 for num in list:
     if num < lower:
         lower = num
+    if num > higher:
+        higher = num
 print('Il voto più basso è ' + str(lower))
+print('Il voto più alto è ' + str(higher))
